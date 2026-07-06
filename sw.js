1551a07f91e06460abbc6959938b4db0b4b1a0b0
Henry Luu <henryluu@hotmail.de>
2026-07-06 19:01:42 +0900

    54123

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nutripilot-v27";
+const CACHE_NAME = "nutripilot-v28";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
@@ -9,30 +9,15 @@ const STATIC_ASSETS = [
   "./favicons/favicon-16x16.png",
   "./favicons/favicon-32x32.png",
   "./favicons/favicon-48x48.png",
-  "./icons/icon-16x16.png",
-  "./icons/icon-32x32.png",
   "./icons/icon-48x48.png",
-  "./icons/icon-64x64.png",
-  "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-167x167.png",
-  "./icons/icon-180x180.png",
+  "./icons/icon-light-48x48.png",
+  "./icons/icon-light-96x96.png",
   "./icons/icon-192x192.png",
-  "./icons/icon-256x256.png",
-  "./icons/icon-384x384.png",
   "./icons/icon-512x512.png",
-  "./icons/icon-1024x1024.png",
-  "./icons/android-circle-192x192.png",
-  "./icons/android-circle-512x512.png",
-  "./icons/android-circle-1024x1024.png",
   "./icons/maskable-192x192.png",
   "./icons/maskable-512x512.png",
-  "./icons/maskable-1024x1024.png",
-  "./icons/apple-touch-icon.png",
-  "./icons/apple-touch-icon-180x180.png"
+  "./icons/apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
